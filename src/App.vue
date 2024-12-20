ad19557251bfe2dd4e3e2d618df2a1c429ce18cb
Diego Rivas <darivas@uce.edu.ec>
2024-12-20 00:46:45 +0000

    props en VUE

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
 
   <img src="./assets/logo.png" alt="vue.logo">
-  <Contador/>
-
-  <Contador/>
-
-
+ 
+  <!--
+  <Contador encabezado="Contador Primero" v-bind:valor="1+1"/>
+  <Contador encabezado="Contador Primero" :valor="1+1"/>
+  <Contador encabezado="Contador Segundo" valor="5+1"/>
+-->
+<Contador encabezado="Hola" :valor="calcular(1,3)" :esVerdad="muestra" :valor2="667"/>
+<Contador  v-bind:valor="calcular(5,5)" :esVerdad="muestra2" />
+<button @:click="cambiar(1)">Cambiar Primero</button>
+<button @:click="cambiar(2)">Cambiar Segundo</button>
   
 </template>
 
@@ -17,7 +22,28 @@ export default {
   components: {
     Contador
   
-  }
+  },
+  //ESTO NO DEBERIA PROGRAMARSE EN EL APP
+  methods:{
+    calcular(a,b){
+      return a+b;
+    },
+    cambiar(valor){
+      if(valor===1){
+        this.muestra=!this.muestra;
+      }else{
+        this.muestra2=!this.muestra2;
+      }
+      
+
+    }
+  },
+  data(){
+        return{
+          muestra:true,
+          muestra2:true
+        };
+    },
 }
 </script>
 
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,7 +1,7 @@
 <template>
 
-<h2>{{titulo}}</h2>
 <!--metods
+<h2>{{titulo}}</h2>
 <p>{{numero}} <sup>2</sup>={{ calcularCuadrado() }}</p>
 <p>{{numero}} <sup>2</sup>={{ calcularCuadrado() }}</p>
 <p>{{numero}} <sup>2</sup>={{ calcularCuadrado() }}</p>
@@ -12,16 +12,18 @@
 <!--Propieda Computada-->
 <div class="prop-Com">
 
+<h2>{{encabezado}} : {{valor2}}</h2>
+<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
+<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
 
-<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
-<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
-<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
-<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
-<p>{{numero}} <sup>2</sup>={{ calcularCuadradoComputado }}</p>
 </div>
 <div class="botones">
     <button class="mas" v-on:click="incremetar()">+ 1</button>
     <button class="menos"  v-on:click="decremetar()">- 1</button>
+</div>
+
+<div v-if="esVerdad">
+    <h2 style="background: wheat;">FELIZ NAVIDAD</h2>
 </div>
 
 
@@ -34,7 +36,7 @@ export default {
  
     data(){
         return{
-            numero : 15,
+            numero : this.valor,
             titulo:'Contador'
         }
     },
@@ -57,7 +59,43 @@ export default {
         return this.numero*this.numero;
 
         }
-    }
+    },
+
+    //Otra forma de declarar el propr y yo puedo especificar el tipo de dato.
+    props:{
+    encabezado:{
+        type:String,
+        required:false,
+        default:"Texto",
+        validator(value){
+           if(!value.includes==='h'){
+            return value;
+           }
+        }
+    },
+    valor: Number,
+    valor2:{
+        type:Number,
+        required:false,
+        default:666,
+        validator(value){
+            //programo mi validacion bajo mi criterio y retorno TRUE cuando cumple
+            // Y retorno FALSE cuando no cumple para mi.
+            return value<=666;
+        }
+
+    },
+    esVerdad:{
+        type: Boolean,
+        required:true
+    },
+    arreglo:Array,
+    fecha:Date,
+    persona:Object     
+}
+    
+
+   // props:['encabezado','valor']
     
 
 }
@@ -74,6 +112,7 @@ button{
     border-radius: 5px;
     border: 1px solid black;
     cursor: pointer;
+    font-size: 16px;
    
 }
 .mas{
